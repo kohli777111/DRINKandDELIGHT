2f45e84f5514db631172d35f6ce8b7e8860e70a7
8859544542 <v885954k@gmail.com>
2020-09-13 17:23:56 +0530

    Create IProductOrderService.java

---
+++ Dink_and_Delight/src/main/java/com/capgemini/dad/services/IProductOrderService.java
@@ -0,0 +1,10 @@
+package com.capgemini.dad.services;
+
+import com.capgemini.dad.entity.ProductOrder;
+
+public interface IProductOrderService {
+	
+	ProductOrder findById(Long orderId);
+	ProductOrder save(ProductOrder productOrder);
+
+}
