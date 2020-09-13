4dbb0a140f62cee27c4b85cfb3d60ab9aa5a0012
8859544542 <v885954k@gmail.com>
2020-09-13 17:23:49 +0530

    Create ProductNotFoundException.java

---
+++ Dink_and_Delight/src/main/java/com/capgemini/dad/exceptions/ProductNotFoundException.java
@@ -0,0 +1,11 @@
+package com.capgemini.dad.exceptions;
+
+public class ProductNotFoundException extends RuntimeException{
+	
+	public ProductNotFoundException(String msg)
+	{
+		super(msg);
+	}
+
+	
+}
