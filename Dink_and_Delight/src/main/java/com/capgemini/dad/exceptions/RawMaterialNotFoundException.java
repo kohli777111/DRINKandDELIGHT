1d01efb69666e3a6278e3c7c19044f98e9e61fc5
8859544542 <v885954k@gmail.com>
2020-09-13 20:25:46 +0530

    Create RawMaterialNotFoundException.java

---
+++ Dink_and_Delight/src/main/java/com/capgemini/dad/exceptions/RawMaterialNotFoundException.java
@@ -0,0 +1,9 @@
+package com.capgemini.dad.exceptions;
+
+public class RawMaterialNotFoundException extends RuntimeException{
+
+	public RawMaterialNotFoundException(String msg)
+	{
+		super(msg);
+	}
+}
