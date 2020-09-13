79ecb2d051b2cb9f99a2f405e4107a769af70a9d
8859544542 <v885954k@gmail.com>
2020-09-13 20:25:39 +0530

    Update RawMaterialOrder.java

--- Dink_and_Delight/src/main/java/com/capgemini/dad/entity/RawMaterialOrder.java
+++ Dink_and_Delight/src/main/java/com/capgemini/dad/entity/RawMaterialOrder.java
@@ -51,5 +51,95 @@ public class RawMaterialOrder {
 	
 	@Column(name="warehouseId", nullable=false)
 	private Long warehouseID;
+
+	public Long getRmorderId() {
+		return rmorderId;
+	}
+
+	public void setRmorderId(Long rmorderId) {
+		this.rmorderId = rmorderId;
+	}
+
+	public String getRmName() {
+		return rmName;
+	}
+
+	public void setRmName(String rmName) {
+		this.rmName = rmName;
+	}
+
+	public Long getSupplierId() {
+		return supplierId;
+	}
+
+	public void setSupplierId(Long supplierId) {
+		this.supplierId = supplierId;
+	}
+
+	public double getQuantityValue() {
+		return quantityValue;
+	}
+
+	public void setQuantityValue(double quantityValue) {
+		this.quantityValue = quantityValue;
+	}
+
+	public String getQuantityUnit() {
+		return quantityUnit;
+	}
+
+	public void setQuantityUnit(String quantityUnit) {
+		this.quantityUnit = quantityUnit;
+	}
+
+	public LocalDate getDateOfOrder() {
+		return dateOfOrder;
+	}
+
+	public void setDateOfOrder(LocalDate dateOfOrder) {
+		this.dateOfOrder = dateOfOrder;
+	}
+
+	public LocalDate getDateOfDelivery() {
+		return dateOfDelivery;
+	}
+
+	public void setDateOfDelivery(LocalDate dateOfDelivery) {
+		this.dateOfDelivery = dateOfDelivery;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public double getPricePerUnit() {
+		return pricePerUnit;
+	}
+
+	public void setPricePerUnit(double pricePerUnit) {
+		this.pricePerUnit = pricePerUnit;
+	}
+
+	public DeliveryStatus getDeliveryStatus() {
+		return deliveryStatus;
+	}
+
+	public void setDeliveryStatus(DeliveryStatus deliveryStatus) {
+		this.deliveryStatus = deliveryStatus;
+	}
+
+	public Long getWarehouseID() {
+		return warehouseID;
+	}
+
+	public void setWarehouseID(Long warehouseID) {
+		this.warehouseID = warehouseID;
+	}
+	
+	
 	
 }
